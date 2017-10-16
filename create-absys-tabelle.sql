40fe0a1319ec5242237b9ed56982bd77b01a52b2
Christian Wirth <wirth@outrightsolutions.de>
2017-10-16 15:46:54 +0100

    fixes stuff

--- create-absys-tabelle.sql
+++ create-absys-tabelle.sql
@@ -1,10 +1,9 @@
-CREATE TABLE IF NOT EXISTS absystest2 (
-    CMID INT(20) NOT NULL AUTO_INCREMENT,
-    subject VARCHAR(45) DEFAULT NULL,
-    nlaenge INT(20),
-    nhoehe INT(20),
-    ntiefe INT(20),
-    nfaktor1 FLOAT(20),
-    cnormtemp1 VARCHAR(50),
+CREATE TABLE IF NOT EXISTS absys(
+    CMID INT(20) NOT NULL ,
+    laenge INT(20),
+    hoehe INT(20),
+    tiefe INT(20),
+    faktor FLOAT(20),
+    norm VARCHAR(50),
     PRIMARY KEY (CMID)
 )
